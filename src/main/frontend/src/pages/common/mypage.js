0cddeb6874bd6e3948cf80b34a308741291ba690
연지 <154227258+yeonji111@users.noreply.github.com>
2024-06-28 15:42:13 +0900

    06.28 | 마이페이지 디자인 | 박연지

--- src/main/frontend/src/pages/common/mypage.js
+++ src/main/frontend/src/pages/common/mypage.js
@@ -354,7 +354,7 @@ export default function Mypage() {
                                 <div className="media-left pr30">
                                     <a>
                                         <div className="mt-2"
-                                             style={{width: "200px", height: "200px", display: "flex"}}>
+                                             style={{width: "300px", height: "300px", display: "flex"}}>
                                             {prevProfile
                                                 ? <img src={prevProfile} style={{
                                                     borderRadius: "50%",
@@ -385,8 +385,6 @@ export default function Mypage() {
                                                     d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm40-120q33 0 56.5-23.5T760-320q0-33-23.5-56.5T680-400q-33 0-56.5 23.5T600-320q0 33 23.5 56.5T680-240ZM400-560q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Z"/>
                                             </svg>
                                             {memberNickname}님 환영합니다
-
-
                                         </li>
                                         <li className="rundry">아이디 : {memberView.memberId}</li>
                                         <li className="rundry">이 름 : {memberView.memberName}</li>
@@ -569,7 +567,7 @@ export default function Mypage() {
                                                         </div>
                                                         <label htmlFor="memberTel"
                                                                className="block rundry text-lg font-medium leading-6 text-gray-900 mt-9">
-                                                            프로필 사진
+                                                            프로필 사진 변경
                                                         </label>
                                                         <div className="mt-2">
                                                             {prevProfile
@@ -696,7 +694,7 @@ export default function Mypage() {
 
                                                     <button
                                                         onClick={saveMemberData}
-                                                        className="btn btn-dark rundry rounded-xl h-10 w-28 mt-7"
+                                                        className="btn btn-dark rundry rounded-xl h-10 w-28 mt-7 ml-40"
                                                     >
                                                         저장
                                                     </button>
